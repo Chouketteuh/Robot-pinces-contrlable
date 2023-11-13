e6d7067d925fc0dbe7e1dfaa38ab8ea9c318a5a5
Thib <54439541+Chouketteuh@users.noreply.github.com>
2023-11-13 09:39:13 +0100

    Update bridge.ino

--- bridge.ino
+++ bridge.ino
@@ -1,1 +1,48 @@
+#include <SoftwareSerial.h>
 
+//Attribution de noms aux PIN
+#define RX_PIN 8
+#define TX_PIN 9
+
+char Data[4];
+
+SoftwareSerial UART = SoftwareSerial(RX_PIN, TX_PIN); //Création du nouveau port serie
+
+void setup()
+{ 
+  pinMode(RX_PIN, INPUT);
+  pinMode(TX_PIN, OUTPUT);
+
+  //Demarrage des ports serie + vitesse de transmission
+  UART.begin(9600);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  if (UART.available() > 0) 
+  {
+    for(int i=0;i<4;i++) //fois 4
+      {
+        Data[i] = UART.read();
+        Serial.print(Data[i]);
+      }
+    if (UART.read() == 'P')
+    {
+      
+      if (UART.read() == 'B')
+      {
+        
+      }
+      else if (UART.read() == 'X')
+      {
+        
+      }
+      else if (UART.read() == 'Y')
+      {
+        
+      }
+    }
+  }
+  delay(200);
+}
