8342494ab264e28c60f024ed727f9f184e7e0579
Thib <54439541+Chouketteuh@users.noreply.github.com>
2023-11-14 09:52:28 +0100

    Update robot.ino

--- robot.ino
+++ robot.ino
@@ -1,3 +1,4 @@
+//Bibliothèques
 #include <stdio.h>
 #include <Servo.h>
 
@@ -5,25 +6,25 @@ int cmd;                    // Variable de stockage de la commande
 int ServoPin = 9;           // Servo PIN
 int temps = 1500;           // Variable de l'inclinaison du servo - Valeur du milieu 1500
 int SRH = 2400, SRAH = 700; // Valeur de rotation max (horaire & anti-horaire)
-Servo ServoMoteur;
+Servo ServoMoteur;          // Déclaration du ServoMoteur
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(9600);                   // Démmarage du monieur serie
   ServoMoteur.attach(ServoPin);         // Déclaration la broche de contrôle du servo
   ServoMoteur.writeMicroseconds(temps); // Démarre à la une valeur de la moitié
 }
 
 void loop()
 {
-  cmd = Serial.read();
-  //Serial.println(temps);             // Retour sur la console pour savoir où on est rendu
-  if(cmd == '0')             // Si la valeur du capteur gauche est en dessous de celui de droite
+  cmd = Serial.read();     // Lecture du moniteur serie et ajout de cette valeur dans la variable
+  //Serial.println(temps); // Retour sur la console pour savoir où on est rendu
+  if(cmd == '0')           // Si la valeur lue du moniteur serie est égale à 0
   {
-    if(temps > SRAH)             // Si n'est pas à la valeur max anti-horaire (600)
+    if(temps > SRAH)       // Si n'est pas à la valeur max anti-horaire (700)
     {
-      temps -= 100;             // Retire 50 à la valeur "temps"
-      Serial.print(temps);      // Affiche la valeur actuelle du servo-moteur 
+      temps -= 100;        // Retire 50 à la valeur "temps"
+      Serial.print(temps); // Affiche la valeur actuelle du servo-moteur 
       Serial.print(" | Sens anti-horaire\n"); // Affiche que la servo-moteur tourne dans le sens anti-horaire
       ServoMoteur.writeMicroseconds(temps);   // On modifie la consigne du servo (temps)
     }
@@ -32,12 +33,12 @@ void loop()
       Serial.print("Rotation max atteinte\n"); // Affiche que la valeur max est atteinte
     }
   }
-  else if(cmd == '1')         // Si la valeur du capteur gauche est au dessus de celui de droite
+  else if(cmd == '1')      // Si la valeur lue du moniteur serie est égale à 0
   {
-    if(temps < SRH)               // Si n'est pas à la valeur max horaire (2400)
+    if(temps < SRH)        // Si n'est pas à la valeur max horaire (2400)
     {
-      temps += 100;              // Ajoute 50 à la valeur "temps"
-      Serial.print(temps);       // Affiche la valeur actuelle du servo-moteur 
+      temps += 100;        // Ajoute 50 à la valeur "temps"
+      Serial.print(temps); // Affiche la valeur actuelle du servo-moteur 
       Serial.print(" | Sens horaire\n");    // Affiche que la servo-moteur tourne dans le sens horaire
       ServoMoteur.writeMicroseconds(temps); // On modifie la consigne du servo (temps)
     }
