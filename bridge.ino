451d1e1007528acabef673305ad832614fe4d513
Thib <54439541+Chouketteuh@users.noreply.github.com>
2023-11-27 09:30:29 +0100

    Update bridge.ino

--- bridge.ino
+++ bridge.ino
@@ -15,15 +15,15 @@ char Data;
 SoftwareSerial UART = SoftwareSerial(RX_PIN, TX_PIN); //Création du nouveau port serie
 
 void setup()
-{ 
+{
+  // Déclare la broche 8 en ENTREE et la broche 9 en sortie
   pinMode(RX_PIN, INPUT);
   pinMode(TX_PIN, OUTPUT);
 
-  //Demarrage des ports serie + vitesse de transmission
-  UART.begin(19200);
-  Serial.begin(9600);
-
+  // Demarrage des ports serie + vitesse de transmission
+  UART.begin(19200); 
   Wire.begin(); // Bus I2C (Pas besoin d'adresse pour le maitre)
+  Serial.begin(9600);
 
   delay(2000);
 }
@@ -32,52 +32,52 @@ void loop()
 {
   while(UART.available() > 0) // Lecture de la file d'attente (buffer) du port série, 0 si aucun caractère n'est disponible.
   {
-    do
+    do // Entre dans la boucle au moins une fois
     {
-      Data = UART.read();
+      Data = UART.read(); // Lecture d'un bit dans la memoire tampon du software serial
       if(Data == 0x58) // Si égal à X
       {
         DataX = UART.parseInt(); // Lit le premier entier disponible dans le buffer série
-        Serial.print(DataX);     // Affiche l'entier stocker dans DataX (Position Potentiomètre X)
-        Serial.println(":X");
+        Serial.print(DataX);     // Affiche l'entier stocké dans DataX (Position Potentiomètre X) dans le moniteur serie
+        Serial.println(":X");    // Affiche un X dans le moniteur serie
       }
       Data = UART.read();
       if(Data == 0x59) // Si égal à Y
       {
         DataY = UART.parseInt(); // Lit le premier entier disponible dans le buffer série
-        Serial.print(DataY);     // Affiche l'entier stocker dans DataY (Position Potentiomètre Y)
-        Serial.println(":Y");
+        Serial.print(DataY);     // Affiche l'entier stocké dans DataY (Position Potentiomètre Y) dans le moniteur serie
+        Serial.println(":Y");    // Affiche un Y dans le moniteur serie
       }
     }while(Data == 0x58 || Data == 0x59); // Tant que c'est égal à X ou Y
     
-    Data = UART.read();
-    if(Data == 0x41)     // Si égal à A
+    Data = UART.read();   // Lecture d'un bit dans la memoire tampon du software serial
+    if(Data == 0x41)      // Si égal à A
     {
-      Serial.println(Data);
+      Serial.println(Data); // Affiche l'entier stocké dans Data (Boutons) dans le moniteur serie
     }
     else if(Data == 0x42) // Si égal à B
     {
-      Serial.println(Data);
+      Serial.println(Data); // Affiche l'entier stocké dans Data (Boutons) dans le moniteur serie
     }
     else if(Data == 0x43) // Si égal à C
     {
-      Serial.println(Data);
+      Serial.println(Data); // Affiche l'entier stocké dans Data (Boutons) dans le moniteur serie
     }
     else if(Data == 0x44) // Si égal à D
     {
-      Serial.println(Data);
+      Serial.println(Data); // Affiche l'entier stocké dans Data (Boutons) dans le moniteur serie
     }
     else if(Data == 0x45) // Si égal à E
     {
-      Serial.println(Data);
+      Serial.println(Data); // Affiche l'entier stocké dans Data (Boutons) dans le moniteur serie
     }
     else if(Data == 0x46) // Si égal à F
     {
-      Serial.println(Data);
+      Serial.println(Data); // Affiche l'entier stocké dans Data (Boutons) dans le moniteur serie
     }
     else if(Data == 0x4A) // Si égal à J
     {
-      Serial.println(Data);
+      Serial.println(Data); // Affiche l'entier stocké dans Data (Boutons) dans le moniteur serie
     } 
   }
 }
